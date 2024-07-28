aa48c60b1fd5557cf0fa6b70f687c9cf6e1b0e50
Luvkush_Sharma <121566305+LuvkushSharma@users.noreply.github.com>
2024-07-28 17:47:42 +0530

    Update userAPI.js

--- Frontend/src/helper/userAPI.js
+++ Frontend/src/helper/userAPI.js
@@ -2,10 +2,9 @@ import axios from "axios";
 
 const fetchUserData = async () => {
   try {
-    const res = await axios.get("https://urban-artmap.onrender.com/api/v1/users/profile", {headers: {
-      'Access-Control-Allow-Origin': '*', 
-      'Content-Type': 'application/json'
-  }, withCredentials: true });
+    const res = await axios.get("https://urban-artmap.onrender.com/api/v1/users/profile", {
+      withCredentials: true,
+    });
     return res.data.data.user;
   } catch (error) {
     console.error("Error fetching user data:", error);
